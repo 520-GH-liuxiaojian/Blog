66c70ee0c293d24eaa626dd4eae9ac057295655e
xiaojian <oppor11520@gmail.com>
2021-03-09 23:09:08 +0800

    feat: node webpack jest

--- 设计模式/装饰器模式/webpack/src/index.js
+++
@@ -1,8 +0,0 @@
-function log(target, name, descriptor) {
-    let oldValue = descriptor.value
-    descriptor.value = function () {
-        console.log(`calling ${name} width`, arguments)
-        return oldValue.apply(this, arguments)
-    }
-    return descriptor
-}
